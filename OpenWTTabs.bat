6189171961aa899f1aed9c77ba4e2793a3b40c84
Leven <phleven@deloitte.com>
2025-03-27 08:18:17 -0400

    Automate WT tabs for ELRR.

---
+++ OpenWTTabs.bat
@@ -0,0 +1,7 @@
+rem Open 6 WT tabs
+wt -w 0 nt -d C:\Users\phleven\git\elrrserviceentities
+wt -w 0 nt -d C:\Users\phleven\git\elrrexternalservices
+wt -w 0 nt -d C:\Users\phleven\git\elrrdatasync
+wt -w 0 nt -d C:\Users\phleven\git\elrraggregator
+wt -w 0 nt -d C:\Users\phleven\git\elrrservices
+wt -w 0 nt -d C:\Users\phleven\git\elrr_documentation 
