fb6a8f39c301bec6af2965e5926f2fc75e1331c7
1Phil <1philleven@gmail,com>
2025-03-27 11:45:19 -0400

    Update documentation.

--- OpenWTTabs.bat
+++ OpenWTTabs.bat
@@ -1,7 +1,7 @@
 rem Open 6 WT tabs
-wt -w 0 nt -d C:\Users\phleven\git\elrrserviceentities
-wt -w 0 nt -d C:\Users\phleven\git\elrrexternalservices
-wt -w 0 nt -d C:\Users\phleven\git\elrrdatasync
-wt -w 0 nt -d C:\Users\phleven\git\elrraggregator
-wt -w 0 nt -d C:\Users\phleven\git\elrrservices
-wt -w 0 nt -d C:\Users\phleven\git\elrr_documentation 
+wt -w 0 nt -d C:\Users\phleven\git\elrrserviceentities --title "elrrserviceentities"
+wt -w 0 nt -d C:\Users\phleven\git\elrrexternalservices --title "elrrexternalservices"
+wt -w 0 nt -d C:\Users\phleven\git\elrrdatasync --title "elrrdatasync"
+wt -w 0 nt -d C:\Users\phleven\git\elrraggregator --title "elrraggregator"
+wt -w 0 nt -d C:\Users\phleven\git\elrrservices --title "elrrservices"
+wt -w 0 nt -d C:\Users\phleven\git\elrr_documentation --title "elrr_documentation"
--- OpenWTTabsPhil.bat
+++ OpenWTTabsPhil.bat
@@ -1,7 +1,7 @@
 rem Open 6 WT tabs
-wt -w 0 nt -d C:\Users\1phil\git\elrrserviceentities
-wt -w 0 nt -d C:\Users\1phil\git\elrrexternalservices
-wt -w 0 nt -d C:\Users\1phil\git\elrrdatasync
-wt -w 0 nt -d C:\Users\1phil\git\elrraggregator
-wt -w 0 nt -d C:\Users\1phil\git\elrrservices
-wt -w 0 nt -d C:\Users\1phil\git\elrr_documentation 
+wt -w 0 nt -d C:\Users\1phil\git\elrrserviceentities --title "elrrserviceentities"
+wt -w 0 nt -d C:\Users\1phil\git\elrrexternalservices --title "elrrexternalservices"
+wt -w 0 nt -d C:\Users\1phil\git\elrrdatasync --title "elrrdatasync"
+wt -w 0 nt -d C:\Users\1phil\git\elrraggregator --title "elrraggregator"
+wt -w 0 nt -d C:\Users\1phil\git\elrrservices --title "elrrservices"
+wt -w 0 nt -d C:\Users\1phil\git\elrr_documentation --title "elrr_documentation"
