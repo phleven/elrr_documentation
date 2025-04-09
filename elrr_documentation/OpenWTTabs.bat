ccb86c194d7f8ad7ad9f060d0414f5b5ddfd0eed
Leven <phleven@deloitte.com>
2025-04-09 11:11:44 -0400

    Update documentation.

--- elrr_documentation/OpenWTTabs.bat
+++
@@ -1,7 +0,0 @@
-rem Open 6 WT tabs
-wt -w 0 nt -d C:\Users\phleven\git\elrrserviceentities --title "elrrserviceentities"
-wt -w 0 nt -d C:\Users\phleven\git\elrrexternalservices --title "elrrexternalservices"
-wt -w 0 nt -d C:\Users\phleven\git\elrrdatasync --title "elrrdatasync"
-wt -w 0 nt -d C:\Users\phleven\git\elrraggregator --title "elrraggregator"
-wt -w 0 nt -d C:\Users\phleven\git\elrrservices --title "elrrservices"
-wt -w 0 nt -d C:\Users\phleven\git\elrr_documentation --title "elrr_documentation"
--- elrr_documentation/OpenWTTabsPhil.bat
+++
@@ -1,7 +0,0 @@
-rem Open 6 WT tabs
-wt -w 0 nt -d C:\Users\1phil\git\elrrserviceentities --title "elrrserviceentities"
-wt -w 0 nt -d C:\Users\1phil\git\elrrexternalservices --title "elrrexternalservices"
-wt -w 0 nt -d C:\Users\1phil\git\elrrdatasync --title "elrrdatasync"
-wt -w 0 nt -d C:\Users\1phil\git\elrraggregator --title "elrraggregator"
-wt -w 0 nt -d C:\Users\1phil\git\elrrservices --title "elrrservices"
-wt -w 0 nt -d C:\Users\1phil\git\elrr_documentation --title "elrr_documentation"
