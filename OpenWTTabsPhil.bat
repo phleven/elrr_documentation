ad13f574a830f4f40fe8b4199b5e224f4906e9d7
Leven <phleven@deloitte.com>
2025-10-10 15:23:42 -0400

    Update documentation.

--- OpenWTTabsPhil.bat
+++ OpenWTTabsPhil.bat
@@ -1,7 +1,7 @@
 rem Open 6 WT tabs
-wt -w 0 nt -d "C:\Users\1phil\git\elrrserviceentities" --title "elrrserviceentities"
-wt -w 0 nt -d "C:\Users\1phil\git\elrrexternalservices" --title "elrrexternalservices"
-wt -w 0 nt -d "C:\Users\1phil\git\elrrdatasync" --title "elrrdatasync"
-wt -w 0 nt -d "C:\Users\1phil\git\elrraggregator" --title "elrraggregator"
-wt -w 0 nt -d "C:\Users\1phil\git\elrrservices" --title "elrrservices"
+wt -w 0 nt -d "C:\Users\1phil\git\elrr-services-entities" --title "elrrserviceentities"
+wt -w 0 nt -d "C:\Users\1phil\git\elrr-external-services" --title "elrrexternalservices"
+wt -w 0 nt -d "C:\Users\1phil\git\elrr-datasync" --title "elrrdatasync"
+wt -w 0 nt -d "C:\Users\1phil\git\elrr-aggregator" --title "elrraggregator"
+wt -w 0 nt -d "C:\Users\1phil\git\elrr-services" --title "elrrservices"
 wt -w 0 nt -d "C:\Users\1phil\git\elrr_documentation" --title "elrr_documentation"
